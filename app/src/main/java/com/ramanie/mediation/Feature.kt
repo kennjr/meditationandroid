f4e289be453dc04b6407188d2dc37d4a87d884b8
kenedy <kenedy@myhechar.pro>
2022-11-03 13:04:05 +0300

    created a data class for the feature item

---
+++ app/src/main/java/com/ramanie/mediation/Feature.kt
@@ -0,0 +1,7 @@
+package com.ramanie.mediation
+
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+
+data class Feature(val title:String, @DrawableRes val iconId: Int, val lightColor: Color,
+                   val mediumColor : Color, val darkColor: Color )
