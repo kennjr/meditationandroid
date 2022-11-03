9ac34e57ad4f151b9d21fa650a3e7bb057d7e76d
kenedy <kenedy@myhechar.pro>
2022-11-03 13:00:58 +0300

    intro'd a couple new constants

--- app/src/main/java/com/ramanie/mediation/Constants.kt
+++ app/src/main/java/com/ramanie/mediation/Constants.kt
@@ -2,3 +2,6 @@ package com.ramanie.mediation
 
 const val DEFAULT_PADDING = 15
 const val ROUNDED_CORNER_RADIUS = 20
+const val ITEM_ROUNDED_CORNER_RADIUS = 10
+
+const val FEATURE_ENTRIES_PER_ROW = 2
